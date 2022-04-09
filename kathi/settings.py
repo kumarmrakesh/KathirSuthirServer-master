4a7a853980305f481ab6d0cb8feb276f0a864f89
Rakesh Kumar <89004140+kumarmrakesh@users.noreply.github.com>
2022-04-09 23:21:44 +0530

    Update settings.py

--- kathi/settings.py
+++ kathi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%o$mxmomzgspyiw8+%nrna108h68h$w6!+e1+=x!3@0k#c5c6$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
